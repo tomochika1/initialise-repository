562ab6a0c8b6b02e6192e11cbee926ae5a19af54
Guest_ <Guest_@Hiroakis-MacBook-Pro.local>
2019-06-20 11:59:21 +0900

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world!"
+  end
+    #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
+
 end
